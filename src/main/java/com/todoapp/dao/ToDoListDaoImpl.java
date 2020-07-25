f2499c66fab5f36dea8a56f6bb7e2c4aaeb1e5b8
deenanath <deenanathmahato.cse@gmail.com>
2020-07-25 12:08:45 +0530

    made it repository using @Repository annotation

--- src/main/java/com/todoapp/dao/ToDoListDaoImpl.java
+++ src/main/java/com/todoapp/dao/ToDoListDaoImpl.java
@@ -2,9 +2,11 @@ package com.todoapp.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.todoapp.entities.ToDoList;
 
+@Repository
 public class ToDoListDaoImpl implements ToDoListDao {
 
 	@Autowired
