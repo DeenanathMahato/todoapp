2f7d8fc79c5e9fcf7cdad7ea80d0cbc744cb74fa
deenanath <deenanathmahato.cse@gmail.com>
2020-07-25 10:09:39 +0530

    home controller method created to handle home request

--- src/main/java/com/todoapp/controller/ToDoListController.java
+++ src/main/java/com/todoapp/controller/ToDoListController.java
@@ -1,8 +1,21 @@
 package com.todoapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.todoapp.services.ToDoListServices;
 
 @Controller
 public class ToDoListController {
 
+	@Autowired
+	ToDoListServices services;
+	
+	@RequestMapping ("/home")
+	public String home (Model model) {
+		model.addAttribute("greeting", "Welcome to ToDoApp!");
+		return "home";
+	}
 }
