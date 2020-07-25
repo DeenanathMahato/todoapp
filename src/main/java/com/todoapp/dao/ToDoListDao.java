71c463296f6024767e79f9ed4e69fd10d26bc187
deenanath <deenanathmahato.cse@gmail.com>
2020-07-25 12:27:02 +0530

    made it as component

--- src/main/java/com/todoapp/dao/ToDoListDao.java
+++ src/main/java/com/todoapp/dao/ToDoListDao.java
@@ -1,7 +1,10 @@
 package com.todoapp.dao;
 
+import org.springframework.stereotype.Component;
+
 import com.todoapp.entities.ToDoList;
 
+@Component
 public interface ToDoListDao {
 	
 	public int saveToDo (ToDoList toDoList);
