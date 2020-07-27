10cdf6ef76a61ef4fe2b10d6523ccf97f78ca0e0
deenanath <deenanathmahato.cse@gmail.com>
2020-07-27 12:19:46 +0530

    Model attribute added

--- src/main/java/com/todoapp/controller/ToDoListController.java
+++ src/main/java/com/todoapp/controller/ToDoListController.java
@@ -3,8 +3,11 @@ package com.todoapp.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.todoapp.entities.ToDoList;
 import com.todoapp.services.ToDoListServices;
 
 @Controller
@@ -15,8 +18,59 @@ public class ToDoListController {
 	
 	@RequestMapping ("/home")
 	public String home (Model model) {
-		System.out.println("home controller fired.");
-		model.addAttribute("greeting", "Welcome to ToDoApp!");
+		
+		model.addAttribute("response", "Welcome to ToDoApp!");
+		
+		ToDoList todoList = new ToDoList ();
+		model.addAttribute("todoHeader", "Write Todo");
+		model.addAttribute("changeBtn", "Save");
+		model.addAttribute("todolist", todoList);
+		
+		return "home";
+	}
+	
+	@RequestMapping (path = "/savetodo", method = RequestMethod.POST)
+	public String saveTodo (@ModelAttribute("todolist") ToDoList todoList, Model model) {
+		
+		System.out.println("savetodo controller fired.");
+		
+		if (todoList.getTitle().isEmpty() )
+		{
+			model.addAttribute("validation", "Plese Enter Title");
+			
+			return "redirect:/home";
+		}
+		else if (todoList.getDescription().isEmpty()) {
+			
+			model.addAttribute("validation", "Plese Enter Description");
+			
+			return "redirect:/home";
+		}
+		
+		int status = this.services.saveToDo(todoList);
+		if (status > 0)
+			model.addAttribute("response", status + " Todo Inserted Successfully!");
+		
+		model.addAttribute("todoHeader", "Write Todo");
+		model.addAttribute("changeBtn", "Save");
+		
+		todoList = null;
+		return "home";
+	}
+	
+	@RequestMapping ("/updatetodo")
+	public String update (Model model) {
+		
+		model.addAttribute("response", "Welcome to ToDoApp!");
+		
+		ToDoList todoList = new ToDoList ();
+		todoList.setTitle("Testing");
+		todoList.setDescription("Yes This is getting...");
+		
+		model.addAttribute("todoHeader", "Update Todo");
+		model.addAttribute("changeBtn", "Update");
+		model.addAttribute("todolist", todoList);
+
 		return "home";
 	}
 }
