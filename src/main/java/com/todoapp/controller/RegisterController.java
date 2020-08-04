19f430fe7cc4bdeea513ba8403fed4de31494a6b
deenanath <deenanathmahato.cse@gmail.com>
2020-08-04 13:43:50 +0530

    formSubmit controller has been created.

--- src/main/java/com/todoapp/controller/RegisterController.java
+++ src/main/java/com/todoapp/controller/RegisterController.java
@@ -1,7 +1,10 @@
 package com.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.todoapp.entities.Student;
 
 @Controller
 public class RegisterController {
@@ -10,5 +13,11 @@ public class RegisterController {
 	public String registerForm () {
 		
 		return "registerform";
+	}
+	
+	@RequestMapping ("/formsubmited")
+	public String submitForm (@ModelAttribute Student student) {
+		System.out.println(student);
+		return "formsuccess";
 	}
 }
