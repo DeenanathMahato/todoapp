bbeaf719eb26a870d9907779d2074c3da63207a3
deenanath <deenanathmahato.cse@gmail.com>
2020-08-04 19:13:30 +0530

    Address class is added.

---
+++ src/main/java/com/todoapp/entities/Address.java
@@ -0,0 +1,47 @@
+package com.todoapp.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "student_address")
+public class Address {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private int id;
+	private String street;
+	private String city;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", city=" + city + "]";
+	}
+}
